2a39aa3fa82972e46e79d3887bcfc51d51ba3489
admin <admin@example.ru>
2026-06-09 10:56:57 +0300

    test133333333331123

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -1,48 +1,45 @@
 def call(Map configParams) {
     pipeline {
-            agent {
-                kubernetes {
-                    yaml """
-            apiVersion: v1
-            kind: Pod
-            metadata:
-            labels:
-                jenkins: agent
-            spec:
-            serviceAccountName: jenkins-kaniko
-            containers:
-            - name: jnlp
-                image: jenkins/inbound-agent:latest
-                args: ['${JENKINS_SECRET}', '${JENKINS_NAME}']
-                env:
-                - name: JENKINS_URL
-                value: "http://jenkins:8080"
+        agent {
+            kubernetes {
+                defaultContainer 'jnlp'
+                yaml """
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    jenkins: agent
+spec:
+  serviceAccountName: default
+  containers:
+  - name: jnlp
+    image: jenkins/inbound-agent:latest
+    args: ['\$(JENKINS_SECRET)', '\$(JENKINS_NAME)']
 
-            - name: python
-                image: python:3.9
-                command: ['tail', '-f', '/dev/null']
+  - name: dind
+    image: docker:dind
+    command: ['dockerd-entrypoint.sh']
+    args: ['--tls=false']
+    securityContext:
+      privileged: true
 
-            - name: tools
-                image: alpine/kubectl:latest
-                command: ['sh', '-c', 'tail -f /dev/null']
+  - name: python
+    image: python:3.9
+    command: ['cat']
+    tty: true
 
-            - name: kaniko   <-- Здесь НЕТ команды!
-                image: gcr.io/kaniko-project/executor:latest
-                volumeMounts:
-                - name: docker-config
-                mountPath: /kaniko/.docker
-                securityContext:
-                runAsUser: 0
-
-            volumes:
-            - name: docker-config
-                secret:
-                secretName: docker-hub-secret
-            """
-                }
+  - name: tools
+    image: alpine/kubectl:latest
+    command:
+    - /bin/sh
+    - -c
+    - |
+      apk add --no-cache curl git yq
+      cat
+    tty: true
+"""
             }
-
-
+        }
 
         environment {
             GIT_CREDENTIALS_ID = 'jenkins_1'
@@ -53,8 +50,11 @@ def call(Map configParams) {
                 steps {
                     container('jnlp') {
                         script {
+
                             checkout scm
+
                             env.BRANCH_NAME = env.BRANCH_NAME ?: 'developer'
+
 
                             def configFile = "${WORKSPACE}/app/.ci-config.yaml"
                             if (!fileExists(configFile)) {
@@ -62,15 +62,20 @@ def call(Map configParams) {
                             }
 
                             def cfg = readYaml(file: configFile)
+
                             if (!cfg || !cfg.appName) {
                                 error("Invalid config: 'appName' is required!")
                             }
 
+  
                             env.FULL_IMAGE_NAME = cfg.dockerImage ?: "docker.io/archcra/${cfg.appName}"
                             env.REPO_URL = cfg.infraRepoUrl ?: 'https://github.com/arch-hcra/st31.git'
                             env.TARGET_PATH = cfg.infraRepoTargetPath ?: 'app-infra/overlays/dev'
                             env.APP_NAME = cfg.appName
+
+
                             env.IMAGE_TAG = env.BRANCH_NAME == 'main' ? 'latest' : "${env.APP_NAME}-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
+                            
                             echo "=== CONFIG LOADED ==="
                             echo "Image: ${env.FULL_IMAGE_NAME}"
                             echo "Tag: ${env.IMAGE_TAG}"
@@ -85,81 +90,80 @@ def call(Map configParams) {
                         sh '''
                             python3 -m venv venv
                             . venv/bin/activate
-                            pip install -r app/requirements.txt
+                            pip install --default-timeout=120 -r app/requirements.txt
                             pytest app/test/test_app.py
                         '''
                     }
                 }
             }
 
-            stage('Build & Push with Kaniko') {
+            stage('Build Docker Image') {
                 steps {
-                    container('kaniko') {
+                    container('dind') {
                         script {
-                            echo "🔍 Building and pushing image: ${env.FULL_IMAGE_NAME}:${env.IMAGE_TAG}"
-
-                            // Подготовка контекста
-                            def contextDir = "${WORKSPACE}/app"
-                            echo "🔍 Context directory: ${contextDir}"
-
-                            // Запускаем Kaniko напрямую
-                            withEnv(["PATH+EXTRA=/kaniko/bin"]) {
-                                sh """
-                                    /kaniko/executor \
-                                    --context=${contextDir} \
-                                    --dockerfile=${contextDir}/Dockerfile \
-                                    --destination=${env.FULL_IMAGE_NAME}:${env.IMAGE_TAG} \
-                                    --verbosity=debug \
-                                    --registry-mirror=https://mirror.gcr.io \
-                                    --use-new-run \
-                                    --single-snapshot
-                                """
-                            }
-
-                            // Если ветка main, создаем тег latest
-                            if (env.BRANCH_NAME == 'main') {
-                                echo "🔍 Tagging as 'latest' for main branch..."
-                                sh """
-                                    /kaniko/executor \
-                                    --context=${contextDir} \
-                                    --dockerfile=${contextDir}/Dockerfile \
-                                    --destination=${env.FULL_IMAGE_NAME}:latest \
-                                    --verbosity=debug
-                                """
-                            }
+                            sh "docker build -t ${env.FULL_IMAGE_NAME}:${env.IMAGE_TAG} -f app/Dockerfile app"
                         }
                     }
                 }
             }
 
-            stage('Update Manifests') {
-                when {
-                    expression { env.BRANCH_NAME == 'main' || env.BRANCH_NAME == 'developer' }
-                }
+            stage('Push Docker Image') {
                 steps {
-                    container('tools') {
+                    container('dind') {
                         script {
-                            withCredentials([string(
-                                credentialsId: 'jenkins_1',
-                                variable: 'GIT_TOKEN'
+                            withCredentials([usernamePassword(
+                                credentialsId: 'docker_token_1',
+                                usernameVariable: 'DOCKER_USER',
+                                passwordVariable: 'DOCKER_PASS'
                             )]) {
                                 sh """
-                                    git clone https://${GIT_TOKEN}@github.com/arch-hcra/st31.git /tmp/infra-repo
-                                    cd /tmp/infra-repo
-                                    git checkout ${env.BRANCH_NAME}
-
-                                    yq eval '.images[0].newTag = \"${env.IMAGE_TAG}\"' ${env.TARGET_PATH}/kustomization.yaml -i
-                                    git config --global user.email "jenkins@ci.local"
-                                    git config --global user.name "Jenkins CI"
-                                    git add ${env.TARGET_PATH}/kustomization.yaml
-                                    git commit -m "chore: update image tag to ${env.IMAGE_TAG} [skip ci]"
-                                    git push https://${GIT_TOKEN}@github.com/arch-hcra/st31.git HEAD:${env.BRANCH_NAME}
+                                    echo \${DOCKER_PASS} | docker login -u \${DOCKER_USER} --password-stdin
+                                    docker push ${env.FULL_IMAGE_NAME}:${env.IMAGE_TAG}
                                 """
+                                
+                                if (env.BRANCH_NAME == 'main') {
+                                    sh """
+                                        docker tag ${env.FULL_IMAGE_NAME}:${env.IMAGE_TAG} ${env.FULL_IMAGE_NAME}:latest
+                                        docker push ${env.FULL_IMAGE_NAME}:latest
+                                    """
+                                }
                             }
                         }
                     }
                 }
             }
+
+        stage('Update Manifests') {
+            when {
+                expression { env.BRANCH_NAME == 'main' || env.BRANCH_NAME == 'developer' }
+            }
+            steps {
+                container('tools') {
+                    script {
+                        withCredentials([string(
+                            credentialsId: 'jenkins_1', 
+                            variable: 'GIT_TOKEN'
+                        )]) {
+                            sh """
+                                git clone https://${GIT_TOKEN}@github.com/arch-hcra/st31.git /tmp/infra-repo
+                                cd /tmp/infra-repo
+                                git checkout ${env.BRANCH_NAME}
+                                
+                                yq eval '.images[0].newTag = "${env.IMAGE_TAG}"' ${env.TARGET_PATH}/kustomization.yaml -i
+
+                                git config --global user.email "jenkins@ci.local"
+                                git config --global user.name "Jenkins CI"
+
+                                git add ${env.TARGET_PATH}/kustomization.yaml
+                                git commit -m "chore: update image tag to ${env.IMAGE_TAG} [skip ci]"
+                                git push https://${GIT_TOKEN}@github.com/arch-hcra/st31.git HEAD:${env.BRANCH_NAME}
+                            """
+                        }
+                    }
+                }
+            }
+}
+
         }
     }
 }
