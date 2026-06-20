179c6a376429ae6da8cb4a6a029889a625cffc8a
admin <admin@example.ru>
2026-06-20 17:58:38 +0300

    reload1111111113333411321323

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -149,14 +149,20 @@ spec:
                                 cd /tmp/infra-repo
                                 git checkout ${env.BRANCH_NAME}
                                 
-                                yq eval '.images[0].newTag = "${env.IMAGE_TAG}"' ${env.TARGET_PATH}/kustomization.yaml -i
+                                CURRENT_TAG=$(yq eval '.images[0].newTag' ${env.TARGET_PATH}/kustomization.yaml 2>/dev/null || echo "")
 
-                                git config --global user.email "jenkins@ci.local"
-                                git config --global user.name "Jenkins CI"
-
-                                git add ${env.TARGET_PATH}/kustomization.yaml
-                                git commit -m "chore: update image tag to ${env.IMAGE_TAG}"
-                                git push https://${GIT_TOKEN}@github.com/arch-hcra/st31.git HEAD:${env.BRANCH_NAME} || exit 0
+                                if [ "$CURRENT_TAG" != "${env.IMAGE_TAG}" ]; then
+                                    echo "Тег в файле: \$CURRENT_TAG, ожидаемый: \$${env.IMAGE_TAG} → обновляем..."
+                                    yq eval '.images[0].newTag = "${env.IMAGE_TAG}"' ${env.TARGET_PATH}/kustomization.yaml -i
+                                    git config --global user.email "jenkins@ci.local"
+                                    git config --global user.name "Jenkins CI"
+                                    git add ${env.TARGET_PATH}/kustomization.yaml
+                                    git commit -m "chore: update image tag to ${env.IMAGE_TAG}"
+                                    git push https://${GIT_TOKEN}@github.com/arch-hcra/st31.git HEAD:${env.BRANCH_NAME}
+                                else
+                                    echo "Тег уже обновлён в файле, пуш отменён"
+                                    exit 0
+                                fi
                             """
                         }
                     }
